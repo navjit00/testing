14eea8a727bef4f0bdbfe3d19bbfeca35598f001
EgorZadorin <egorzadorin04@gmail.com>
2023-10-20 18:11:00 +0200

    merge-issue fix

--- frontend/src/components/Promo.vue
+++ frontend/src/components/Promo.vue
@@ -1,20 +1,12 @@
 <style scoped>
-
-  /* .input-wrapper {
-    @apply flex flex-col h-20 text-gray-300;
-  }
-  input[type=text], select {
-    @apply bg-secondary border border-gray-700 text-gray-300 rounded-lg h-full mt-2 text-sm px-2 py-3;
-  } */
-  img {
-    @apply w-full rounded-lg;
-  }
+img {
+  @apply w-full rounded-lg;
+}
 </style>
 <template>
-  <img src="/long_story.png" alt="long story 1">
   <Splide class="mt-3 z-10" :options="options">
     <SplideSlide>
-      <img src="/banner-1.jpg" alt="Sample 1">
+      <img src="/long_story.png" alt="Sample 1">
     </SplideSlide>
     <SplideSlide>
       <img src="/banner-2.jpg" alt="Sample 2">
@@ -27,3 +19,13 @@
     </SplideSlide>
   </Splide>
 </template>
+<script setup>
+import { Splide, SplideSlide } from '@splidejs/vue-splide'
+import '@splidejs/splide/dist/css/splide.min.css';
+const options = {
+  arrows: false,
+  autoplay: true,
+  perpage: 1,
+  type: 'loop'
+}
+</script>
